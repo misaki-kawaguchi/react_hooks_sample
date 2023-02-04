38df2bbf78cadd8d7e895fe99be340b37b546128
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2023-02-04 18:42:10 +0900

    userStateファンクションコンポーネントに状態を持たせる (#1)
    * incrementを実装
    
    * decrementを追加
    
    * increment2とdecrement2を追加
    
    * resetを追加
    
    * double追加
    
    * divide3を追加

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const increment2 = () => {
+    setCount((previousCount) => previousCount + 1);
+  };
+
+  const decrement2 = () => {
+    setCount((previousCount) => previousCount - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  const double = () => {
+    setCount(count * 2);
+  };
+
+  const divide3 = () =>
+    setCount((previousCount) =>
+      previousCount % 3 === 0 ? previousCount / 3 : previousCount
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <></>で囲ってもok
+    <React.Fragment>
+      <div>count:{count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>+1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>*2</button>
+        <button onClick={divide3}>3の倍数の時だけ3でわる</button>
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
