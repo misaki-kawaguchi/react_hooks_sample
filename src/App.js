736274b5ba5842ffba7e4a539f432c044bafadcc
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2023-02-05 05:44:49 +0900

    複数の状態を管理する (#2)
    * +1、-1、resetできるようにする
    
    * nameを変更できるようにする

--- src/App.js
+++ src/App.js
@@ -1,56 +1,35 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-  const increment2 = () => {
-    setCount((previousCount) => previousCount + 1);
-  };
-
-  const decrement2 = () => {
-    setCount((previousCount) => previousCount - 1);
-  };
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
   const reset = () => {
-    setCount(0);
+    setPrice(props.price);
+    setName(props.name);
   };
-
-  const double = () => {
-    setCount(count * 2);
-  };
-
-  const divide3 = () =>
-    setCount((previousCount) =>
-      previousCount % 3 === 0 ? previousCount / 3 : previousCount
-    );
 
   return (
     // <></>で囲ってもok
     <React.Fragment>
-      <div>count:{count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>+1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>*2</button>
-        <button onClick={divide3}>3の倍数の時だけ3でわる</button>
-      </div>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>reset</button>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
     </React.Fragment>
   );
+};
+
+App.defaultProps = {
+  name: "サンプル",
+  price: 1000,
 };
 
 export default App;
