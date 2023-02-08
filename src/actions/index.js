7a8bc028c0dbf9d9b6566dfa43bb969f1ca39131
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2023-02-08 19:58:44 +0900

    アクションのタイプを定数で定義する (#11)

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+// 定数に入れるとタイポした時にエラーがすぐわかる
+// タイポした時にエラーが出るので、定数で定義する
+export const CREATE_EVENT = "CREATE_EVENT"
+export const DELETE_EVENT = "DELETE_EVENT"
+export const DELETE_ALL_EVENTS = "DELETE_ALL_EVENTS"
--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { CREATE_EVENT, DELETE_ALL_EVENTS } from "../actions"
 
 const EventForm = ({ state, dispatch }) => {
   const [title, setTitle] = useState("")
@@ -9,7 +10,7 @@ const EventForm = ({ state, dispatch }) => {
     e.preventDefault()
     // dispatchを呼ぶ
     dispatch({
-      type: "CREATE_EVENT",
+      type: CREATE_EVENT,
       title,
       body,
     })
@@ -23,7 +24,7 @@ const EventForm = ({ state, dispatch }) => {
     const result = window.confirm(
       "全てのイベントを本当に削除しても良いですか？"
     )
-    if (result) dispatch({ type: "DELETE_ALL_EVENTS" })
+    if (result) dispatch({ type: DELETE_ALL_EVENTS })
   }
 
   const unCreatable = title === "" || body === ""
